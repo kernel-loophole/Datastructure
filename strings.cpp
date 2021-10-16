f568b4a417dbdcea2a2edc2df9f4f024f8227e25
Zulqarnain <76658396+kernel-loophole@users.noreply.github.com>
2021-10-16 15:56:30 -0400

    Add files via upload

---
+++ strings.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    char *ch="123233.3422324234"; 
+    int i;
+    char **ptr;
+    
+   
+   
+                 for( i=0;ch[i]!='\0';++i)
+   {
+               
+            
+}
+   cout<<"Lenth Of ch is :"<<endl;
+   cout<<i<<endl;
+      ptr=&ch;
+      char ***ptr1;
+      ptr1=&ptr;
+    
+   for(int a=0;a<i;a++)
+   {
+      *ptr1++;  
+     if(**ptr1[a]='.')
+           {
+               cout<<"find"<<a<<endl;
+            } 
+            else
+            {
+                
+            }
+}
+
+  return 0;
+
+}
+
+    
+
+
